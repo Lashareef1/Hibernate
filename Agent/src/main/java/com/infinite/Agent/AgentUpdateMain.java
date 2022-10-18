6560763c5a67efb0ffc75fd1c487992c88e29352
Lashareef1 <95951715+Lashareef1@users.noreply.github.com>
2022-10-18 17:34:32 +0530

    Add files via upload

---
+++ Agent/src/main/java/com/infinite/Agent/AgentUpdateMain.java
@@ -0,0 +1,53 @@
+package com.infinite.Agent;
+
+import java.util.List;
+import java.util.Scanner;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.cfg.Configuration;
+
+ 
+public class AgentUpdateMain {
+
+	public static void main(String[] args) {
+
+		Agent agent = new Agent(); 
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("Enter Agent Id");
+		agent.setAgentid(sc.nextInt());
+		System.out.println("Enter Agent Name");
+		agent.setName(sc.next());
+		System.out.println("Enter Agent City");
+		agent.setCity(sc.next());
+		System.out.println("Enter Agent Gender");
+		agent.setGender(Gender.valueOf(sc.next()));
+		System.out.println("Enter Agent MaritalStatus");
+		agent.setMaritalStatus(sc.nextInt());
+		System.out.println("Enter Agent Premium");
+		agent.setPremium(sc.nextDouble());
+		
+		
+		
+		Configuration cfg = new AnnotationConfiguration().configure();
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session session = sf.openSession();
+		Query query = session.createQuery("from Agent where Agentid="+agent.getAgentid());
+		List<Agent> agentList = query.list();
+		session.close();
+		if (agentList.size()==1) {
+			session= sf.openSession();
+			Transaction trans = session.beginTransaction();
+			session.saveOrUpdate(agent);
+			trans.commit();
+			System.out.println("*** Record Update ***");
+		} else {
+			System.out.println("*** Record not Found ***");
+		}
+	}
+	}
+
+
